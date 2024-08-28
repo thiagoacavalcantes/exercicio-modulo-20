f2e650593ed90cb187ecec2997faf7e1eaa9f993
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2024-08-28 16:44:30 -0300

    Solução exercicio módulo 20

---
+++ mdc.js
@@ -0,0 +1,15 @@
+// 1. Calcule o MDC (máximo divisor comum) entre dois números. //
+
+function calcularMDC(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+let numero1 = 20;
+let numero2 = 7453;
+let resultado = calcularMDC(numero1, numero2);
+console.log(`MDC entre ${numero1} e ${numero2}: ${resultado}`);
---
+++ multiplos.js
@@ -0,0 +1,15 @@
+/* 3. Listando todos os números inteiros abaixo de 10 que são múltiplos de 3 ou 5, ficamos com 3, 5, 6 e 9. 
+soma desses múltiplos é 23. Crie uma função que retorne a soma de todos os múltiplos de 5 ou 7 abaixo de 1000.*/
+
+function calcularSomaMultiplos() {
+    let soma = 0;
+    for (let i = 1; i < 1000; i++) {
+        if (i % 5 === 0 || i % 7 === 0) {
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+const result = calcularSomaMultiplos();
+console.log("A soma de todos os múltiplos de 5 ou 7 abaixo de 1000 é: " + result);
